b0d88f912f62cc0d204291160235ae243e9da7a5
Christophe Vuillot <c.vuillot@tudelft.nl>
2019-02-12 17:12:20 +0000

    humhum

--- script_pincode.py
+++ script_pincode.py
@@ -3,8 +3,8 @@
 # import time
 # import scipy.sparse as sp
 # import numpy as np
-from itertools import combinations
-from scipy.sparse import dok_matrix
+# from itertools import combinations
+# from scipy.sparse import dok_matrix
 import QuantumCodeConstruction.pincode as pinco
 # import QuantumCodeConstruction.hypergraphproduct as hp
 from QuantumCodeConstruction.utils import readsparsematrix, writesparsematrix
@@ -46,7 +46,7 @@ if __name__ == "__main__":
     #                                  PTCHECKSX,
     #                                  PTCHECKSZ,
     #                                  PTQUBITS - PTCHECKSX - PTCHECKSZ))
-    TRANSITIONS = [readsparsematrix('BoundaryMaps/535_6840_'+str(j)+'.npz').todense()
+    TRANSITIONS = [readsparsematrix('BoundaryMaps/535_531360_'+str(j)+'.npz').todense()
                    for j in range(3, 0, -1)]
     POSETHP = pinco.GrPoset(TRANSITIONS, iscomplete=False)
     PCX, PCZ = pinco.pincode(POSETHP, 1, 2)
@@ -62,30 +62,30 @@ if __name__ == "__main__":
                                      PCCHECKSX,
                                      PCCHECKSZ,
                                      PCQUBITS - PCCHECKSX - PCCHECKSZ))
-    ALL1COSETS = []
-    for j in range(4):
-        ALL1COSETS += readcosetfile('PCMatrices/535_6840_'+str(j)+'.cos')
-    ALL2COSETS = []
-    for cos1, cos2 in combinations(ALL1COSETS, 2):
-        inter = set(cos1).intersection(set(cos2))
-        if inter:
-            ALL2COSETS.append(list(inter))
-    print(len(ALL1COSETS))
-    print({len(co) for co in ALL1COSETS})
-    print(len(ALL2COSETS))
-    print({len(co) for co in ALL2COSETS})
-    NQ = max([a for sub in ALL1COSETS for a in sub])
-    print(NQ)
-    PCXCOS = dok_matrix((len(ALL1COSETS), NQ), dtype='int')
-    for j, xche in enumerate(ALL1COSETS):
-        for elem in xche:
-            PCXCOS[j, elem-1] = 1
-    PCZCOS = dok_matrix((len(ALL2COSETS), NQ), dtype='int')
-    for j, zche in enumerate(ALL2COSETS):
-        for elem in zche:
-            PCZCOS[j, elem-1] = 1
+    # ALL1COSETS = []
+    # for j in range(4):
+    #     ALL1COSETS += readcosetfile('PCMatrices/535_531360_'+str(j)+'.cos')
+    # ALL2COSETS = []
+    # for cos1, cos2 in combinations(ALL1COSETS, 2):
+    #     inter = set(cos1).intersection(set(cos2))
+    #     if inter:
+    #         ALL2COSETS.append(list(inter))
+    # print(len(ALL1COSETS))
+    # print({len(co) for co in ALL1COSETS})
+    # print(len(ALL2COSETS))
+    # print({len(co) for co in ALL2COSETS})
+    # NQ = max([a for sub in ALL1COSETS for a in sub])
+    # print(NQ)
+    # PCXCOS = dok_matrix((len(ALL1COSETS), NQ), dtype='int')
+    # for j, xche in enumerate(ALL1COSETS):
+    #     for elem in xche:
+    #         PCXCOS[j, elem-1] = 1
+    # PCZCOS = dok_matrix((len(ALL2COSETS), NQ), dtype='int')
+    # for j, zche in enumerate(ALL2COSETS):
+    #     for elem in zche:
+    #         PCZCOS[j, elem-1] = 1
     # TIME = time.localtime()
-    writesparsematrix(PCX, 'PCMatrices/535_6840_X.sms')
-    writesparsematrix(PCZ, 'PCMatrices/535_6840_Z.sms')
-    writesparsematrix(PCXCOS, 'PCMatrices/535_6840_XCOS.sms')
-    writesparsematrix(PCZCOS, 'PCMatrices/535_6840_ZCOS.sms')
+    writesparsematrix(PCX, 'PCMatrices/535_531360_X.sms')
+    writesparsematrix(PCZ, 'PCMatrices/535_531360_Z.sms')
+    # writesparsematrix(PCXCOS, 'PCMatrices/535_531360_XCOS.sms')
+    # writesparsematrix(PCZCOS, 'PCMatrices/535_531360_ZCOS.sms')
