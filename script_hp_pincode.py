e8723a6311ff881738d75044f331672ed84a6368
Christophe Vuillot <c.vuillot@tudelft.nl>
2019-02-22 18:45:40 +0100

    adding script generating some random hp pincodes

---
+++ script_hp_pincode.py
@@ -0,0 +1,26 @@
+"""creating pin codes from hypergraph products
+"""
+import QuantumCodeConstruction.pincode as pinco
+import QuantumCodeConstruction.hypergraphproduct as hp
+from QuantumCodeConstruction.utils import writesparsematrix
+
+
+if __name__ == "__main__":
+    SEED = 1236
+    TRANSITIONS = hp.randomhypergraphproductlist(2, 3, 2, 2, seed=SEED)
+    POSETHP = pinco.GrPoset(TRANSITIONS, iscomplete=False)
+    PCX, PCZ = pinco.pincode(POSETHP, 1, 2)
+    WEIGHTSX = {a for che in PCX.sum(axis=1).tolist() for a in che}
+    WEIGHTSZ = {a for che in PCZ.sum(axis=1).tolist() for a in che}
+    print(WEIGHTSX)
+    print(WEIGHTSZ)
+    print(POSETHP.levelsizes)
+    PCCHECKSX, PCQUBITS = PCX.shape
+    PCCHECKSZ, _ = PCZ.shape
+    print('qubits - xchecks - zchecks = logicals: \n \
+           {} - {} - {} = {}'.format(PCQUBITS,
+                                     PCCHECKSX,
+                                     PCCHECKSZ,
+                                     PCQUBITS - PCCHECKSX - PCCHECKSZ))
+    writesparsematrix(PCX, 'PCMatrices/randomhp_' + str(SEED) + '_X.sms')
+    writesparsematrix(PCZ, 'PCMatrices/randomhp_' + str(SEED) + '_Z.sms')
