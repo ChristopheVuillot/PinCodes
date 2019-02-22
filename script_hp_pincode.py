981deceb0a49f8e3abc398e65be55ee832908907
Christophe Vuillot <c.vuillot@tudelft.nl>
2019-02-22 19:17:27 +0100

    created a bunch off small pincodes from random hp

--- script_hp_pincode.py
+++ script_hp_pincode.py
@@ -7,25 +7,30 @@ from QuantumCodeConstruction.utils import writesparsematrix
 
 
 if __name__ == "__main__":
-    SEED = 1236
-    TRANSITIONS = hp.randomhypergraphproductlist(2, 3, 2, 2, seed=SEED)
-    POSETHP = pinco.GrPoset(TRANSITIONS, iscomplete=False)
-    PCX, PCZ = pinco.pincode(POSETHP, 1, 2)
-    WEIGHTSX = {a for che in PCX.sum(axis=1).tolist() for a in che}
-    WEIGHTSZ = {a for che in PCZ.sum(axis=1).tolist() for a in che}
-    print(WEIGHTSX)
-    print(WEIGHTSZ)
-    print(POSETHP.levelsizes)
-    PCCHECKSX, PCQUBITS = PCX.shape
-    PCCHECKSZ, _ = PCZ.shape
-    print('qubits - xchecks - zchecks = logicals: \n \
-           {} - {} - {} = {}'.format(PCQUBITS,
-                                     PCCHECKSX,
-                                     PCCHECKSZ,
-                                     PCQUBITS - PCCHECKSX - PCCHECKSZ))
+    for SEED in range(1000, 1100):
+        if SEED == 1097:
+            # 1097 is a bad seed ! skip it !
+            continue
+        TRANSITIONS = hp.randomhypergraphproductlist(2, 3, 2, 2, seed=SEED)
+        POSETHP = pinco.GrPoset(TRANSITIONS, iscomplete=False)
+        PCX, PCZ = pinco.pincode(POSETHP, 1, 2)
+        for j, bmap in enumerate(TRANSITIONS):
+            writesparsematrix(bmap, 'BoundaryMaps/randomhp_{}_{}.sms'.format(SEED, j))
+        WEIGHTSX = {a for che in PCX.sum(axis=1).tolist() for a in che}
+        WEIGHTSZ = {a for che in PCZ.sum(axis=1).tolist() for a in che}
+        print(WEIGHTSX)
+        print(WEIGHTSZ)
+        print(POSETHP.levelsizes)
+        PCCHECKSX, PCQUBITS = PCX.shape
+        PCCHECKSZ, _ = PCZ.shape
+        print('qubits - xchecks - zchecks = logicals: \n \
+               {} - {} - {} = {}'.format(PCQUBITS,
+                                         PCCHECKSX,
+                                         PCCHECKSZ,
+                                         PCQUBITS - PCCHECKSX - PCCHECKSZ))
 
-    CSSCOND = (np.dot(PCX.todense(), PCZ.transpose().todense()) % 2).sum() == 0
-    print('This is a valid CSS code: {}'.format(CSSCOND))
+        CSSCOND = (np.dot(PCX.todense(), PCZ.transpose().todense()) % 2).sum() == 0
+        print('This is a valid CSS code: {}'.format(CSSCOND))
 
-    writesparsematrix(PCX, 'PCMatrices/randomhp_' + str(SEED) + '_X.sms')
-    writesparsematrix(PCZ, 'PCMatrices/randomhp_' + str(SEED) + '_Z.sms')
+        writesparsematrix(PCX, 'PCMatrices/randomhp_{}_X.sms'.format(SEED))
+        writesparsematrix(PCZ, 'PCMatrices/randomhp_{}_Z.sms'.format(SEED))
