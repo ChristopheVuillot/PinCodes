d0cfd539c04cda3473b9c76fb78be9b73f1c7a22
Christophe Vuillot <c.vuillot@tudelft.nl>
2019-06-01 22:33:09 +0200

    added a script to compile all nk just to see

---
+++ script_plot_n_k.py
@@ -0,0 +1,17 @@
+import json
+import os
+
+
+PATHPARAM = 'CodeParameters/'
+NKFILE = 'list_n_k.txt'
+NKPATH = PATHPARAM + NKFILE
+FILELIST = os.listdir(PATHPARAM)
+
+print('Creating file: {}'.format(NKPATH))
+with open(NKPATH, 'w') as nkfile:
+    nkfile.write('n k\n')
+    for FILE in FILELIST:
+        print('Fetching and processing file: {}'.format(FILE))
+        with open(FILE, 'r') as paramfile:
+            jsondict = json.load(paramfile)
+            nkfile.write('{} {}\n'.format(jsondict['n'], jsondict['k']))
